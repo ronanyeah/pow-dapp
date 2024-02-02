85718f5ce32e2f51f670766cd9572645b42ecf28
Sense <105939929+0xSense@users.noreply.github.com>
2024-02-01 20:44:28 -0500

    fix: added types to a few parameters and variables to ensure typescript knows indentity

--- src/worker.ts
+++ src/worker.ts
@@ -20,35 +20,38 @@ type Criteria =
       end: string;
     };
 
-onmessage = (event) =>
+  type Counter = number;
+  type KeyPair = CryptoKeyPair;
+
+onmessage = (event: MessageEvent<any>) =>
   (async () => {
     const params: Params = event.data;
 
     const isMatch = (() => {
       const criteria = params.criteria;
       if ("start" in criteria && "end" in criteria) {
-        return (addr: string) =>
+        return (addr: String) =>
           addr.startsWith(criteria.start) && addr.endsWith(criteria.end);
       } else if ("start" in criteria) {
-        return (addr: string) => addr.startsWith(criteria.start);
+        return (addr: String) => addr.startsWith(criteria.start);
       } else {
-        return (addr: string) => addr.endsWith(criteria.end);
+        return (addr: String) => addr.endsWith(criteria.end);
       }
     })();
 
-    let count = 0;
+    let count: Counter = 0;
     const keys: CryptoKeyPair[] = [];
 
     await Promise.all([
       (async () => {
         while (count < params.count) {
           try {
-            const keypair = await crypto.subtle.generateKey("Ed25519", true, [
+            const keypair: KeyPair = await crypto.subtle.generateKey("Ed25519", true, [
               "sign",
               "verify",
             ]);
             keys.push(keypair);
-          } catch (_e) {
+          } catch (_e: unknown) {
             console.error("op1 fail");
           }
           count += 1;
@@ -64,7 +67,7 @@ onmessage = (event) =>
               if (isMatch(addr)) {
                 postMessage({ match: await exportBytes(keypair) });
               }
-            } catch (_e) {
+            } catch (_e: unknown) {
               console.error("op2 fail");
             }
           } else {
